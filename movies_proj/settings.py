09aebaaa8715d616ca051d54e732138c1c698ed9
dazariani <davitazariani@gmail.com>
2024-10-24 18:39:13 +0400

    Added cors origins

--- movies_proj/settings.py
+++ movies_proj/settings.py
@@ -164,5 +164,8 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+  "http://localhost:5173/",
+  "https://movieclub-d9mb.onrender.com"
+]
 CORS_ALLOW_CREDENTIALS = True
